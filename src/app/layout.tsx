0bd19f8491f2e332898f0c4365fbe14c6ddb0ebc
marcus <marvmms@gmail.com>
2025-02-08 13:27:14 -0300

    feat: ajustes no css global

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 import CustomApolloProvider from "@/providers/ApolloProvider";
 import Header from "@/components/Header";
 import { SearchProvider } from "@/context/SearchContext";
+import { Saira } from "next/font/google";
+import "./globals.css";
+
+const saira = Saira({
+  subsets: ["latin"],
+  weight: ["300","400", "600"],
+});
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="pt">
+    <html lang="pt" className={saira.className}>
       <body>
         <CustomApolloProvider>
           <SearchProvider>
